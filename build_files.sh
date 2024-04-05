237ec43c8623646834999d1971ebcdc57775c933
Himanshu72060 <himanshusharma00898@gmail.com>
2024-04-05 12:21:15 +0530

    change

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,2 @@
-# Define variables for commands
-PIP_INSTALL_CMD=pip install -r requirement.txt
-COLLECTSTATIC_CMD=python3.10 manage.py collectstatic
-
-# Execute the commands
-$PIP_INSTALL_CMD
-$COLLECTSTATIC_CMD
+pip install -r requirement.txt
+python3.10 manage.py collectstatic
